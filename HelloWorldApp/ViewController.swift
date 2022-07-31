2f6ad27aee8e28e2e35b3d307031f3c4979c120a
Lera Zhelezinna <valeriiagustova@yandex.ru>
2022-07-31 22:37:04 +0300

    1. Setup UI

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.isHidden = true
+        redView.layer.cornerRadius = 10
+        
+        yellowView.isHidden = true
+        yellowView.layer.cornerRadius = 10
+        
+        greenView.isHidden = true
+        greenView.layer.cornerRadius = 10
+        
     }
-
-
+    
+    @IBAction func startButton() {
+        redView.isHidden.toggle()
+        
+    }
 }
+private func setupButton(with title: String) -> UIButton.Configuration {
+    var buttonConfiguration = UIButton.Configuration.filled()
+    buttonConfiguration.baseBackgroundColor = .white
+    buttonConfiguration.title = title
+    buttonConfiguration.buttonSize = .large
+    buttonConfiguration.cornerStyle = .large
+    buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+    return buttonConfiguration
+}
+
+
+    
 
